1b79a9e08ed4abe50c20e24b7c8ce7a421e862fc
Patrick Kerby <patrickkerby@Patricks-MacBook-Pro.local>
2017-11-02 21:16:55 -0600

    Attach Typekit kit

--- header.php
+++ header.php
@@ -19,6 +19,16 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
 	<script>
+	  (function(d) {
+	    var config = {
+	      kitId: 'nmx3ksc',
+	      scriptTimeout: 3000,
+	      async: true
+	    },
+	    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+	  })(document);
+	</script>
+	<script>
 		new WOW().init(); 
 	</script>
 </head>
