845cc0c34fffbeba5672bb2b8e2d1ba8724e9752
Patrick Kerby <patrickkerby@Patricks-MacBook-Pro.local>
2017-11-19 16:42:05 -0700

    Initial Single Post Page style and structure

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -6,9 +6,9 @@
  */
 
 ?>
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article class="row no-gutters justify-content-center" id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
+	<header class="entry-header wow fadeIn col-8" data-wow-duration="1s" >
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
@@ -20,9 +20,7 @@
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
-	<div class="entry-content">
+	<div class="entry-content col-8">
 
 		<?php the_content(); ?>
 
@@ -34,11 +32,5 @@
 		?>
 
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-
-		<?php understrap_entry_footer(); ?>
-
-	</footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
