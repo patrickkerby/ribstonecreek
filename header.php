7f866ea786093b2ad93c80811f0cf025d89ad943
Patrick Kerby <patrickkerby@Patricks-MBP.hitronhub.home>
2017-11-21 08:56:40 -0700

    Add Hotjar tracking script

--- header.php
+++ header.php
@@ -27,7 +27,17 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<?php } else {
 				//no map
 	}?>
-		
+<!-- Hotjar Tracking Code for http://www.ribstonecreekbrewery.ca -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:698612,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>		
 	<script>
 	  (function(d) {
 	    var config = {
